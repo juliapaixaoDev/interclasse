f64b2a43680f3b542e7b24886ad491b5e5dc6af5
Julia <sospaixao@gmail.com>
2025-05-08 10:28:04 -0300

    ENVIANDO A PRIMEIRA VERSAO DO PROJETO

---
+++ src/main/java/br/senai/sp/interclasse/InterclasseApplication.java
@@ -0,0 +1,13 @@
+package br.senai.sp.interclasse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterclasseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterclasseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/senai/sp/interclasse/InterclasseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senai.sp.interclasse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterclasseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
